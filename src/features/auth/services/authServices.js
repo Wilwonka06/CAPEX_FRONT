1d532cbd7ec0bbeac332718f02a4bf9318059bbc
Wilson Rojas <167899740+Wilwonka06@users.noreply.github.com>
2026-03-31 08:30:49 -0500

    Fix export statement for authService

--- src/features/auth/services/authServices.js
+++ src/features/auth/services/authServices.js
@@ -2,7 +2,7 @@ import { apiRequest } from '../../../shared/config/apiConfig';
 
 const isDev = import.meta.env.DEV;
 
-const authService = {
+export default const authService = {
 
     async register(userData) {
         try {
@@ -93,5 +93,3 @@ const authService = {
         }
     },
 };
-
-export default authService;
